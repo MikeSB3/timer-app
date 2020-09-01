86702b124b66912357d8d1cb96cf943a98d4710c
Michael Berryman <berryman.michael@yahoo.com>
2020-09-01 06:29:06 -0700

    add SVG countdown circle

--- index.js
+++ index.js
@@ -1,63 +1,19 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        if (callbacks) {
-            this.onStart = callbacks.onStart;
-            this.onTick = callbacks.onTick;
-            this.onComplete = callbacks.onComplete;
-        }
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => {
-        if (this.onStart) {
-            this.onStart();
-        }
-        this.tick();
-        this.intervalId = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.intervalId);
-    }
-
-    tick = () => {
-        if (this.timeRemaining <= 0) {
-            this.pause();
-            if (this.onComplete) {
-                this.onComplete();
-            }
-        } else {
-            this.timeRemaining--;
-            if (this.onTick) {
-                this.onTick();
-            }
-        }
-    }
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart() {
         console.log('timer started')
     },
     onTick() {
-        console.log('timer just ticked down')
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+        currentOffset -= 1;
     },
     onComplete() {
         console.log('timer is completed')
